36f295868a93042c068661acce29720f6d8f28f8
AliensToEarth <alsakirova60@gmail.com>
2026-08-23 20:56:11 +0400

    branch 1.21.3 release

--- src/main/java/dev/alienstoearth/paramax/modmenu/ParaMaxConfigScreen.java
+++ src/main/java/dev/alienstoearth/paramax/modmenu/ParaMaxConfigScreen.java
@@ -37,7 +37,6 @@ public final class ParaMaxConfigScreen extends Screen {
             Toggle.of("paramax.toggle.half_rate_texture_animations", c -> c.halfRateTextureAnimations, c -> c.halfRateTextureAnimations = !c.halfRateTextureAnimations),
             Toggle.of("paramax.toggle.reduce_cosmetic_entity_ticks", c -> c.reduceCosmeticEntityTicks, c -> c.reduceCosmeticEntityTicks = !c.reduceCosmeticEntityTicks),
             Toggle.of("paramax.toggle.parallel_entity_visibility", c -> c.parallelEntityVisibility, c -> c.parallelEntityVisibility = !c.parallelEntityVisibility),
-            Toggle.of("paramax.toggle.smart_lightmap", c -> c.smartLightmap, c -> c.smartLightmap = !c.smartLightmap),
             Toggle.of("paramax.toggle.cache_hud_text", c -> c.cacheHudText, c -> c.cacheHudText = !c.cacheHudText),
             Toggle.of("paramax.toggle.adaptive_governor", c -> c.adaptivePerformance, c -> c.adaptivePerformance = !c.adaptivePerformance),
             Toggle.of("paramax.toggle.frame_pacing", c -> c.framePacing, c -> c.framePacing = !c.framePacing),
